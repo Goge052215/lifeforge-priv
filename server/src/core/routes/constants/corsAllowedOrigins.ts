340ac2a60559532ad9869f938644e7f1906785be
goge052215 <hx13651954192@163.com>
2026-06-08 22:45:42 +0800

    feat: add fly deployment config and CORS origins env config

--- server/src/core/routes/constants/corsAllowedOrigins.ts
+++ server/src/core/routes/constants/corsAllowedOrigins.ts
@@ -1,7 +1,17 @@
-export const CORS_ALLOWED_ORIGINS = [
+const DEFAULT_CORS_ALLOWED_ORIGINS = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:4173',
-  'http://localhost',
-  'https://_.melvinchia.dev'
+  'http://localhost'
+]
+
+const extraOrigins = [
+  process.env.CLIENT_ORIGIN,
+  ...(process.env.ADDITIONAL_CORS_ORIGINS?.split(',') ?? [])
+]
+  .map(origin => origin?.trim())
+  .filter((origin): origin is string => Boolean(origin))
+
+export const CORS_ALLOWED_ORIGINS = [
+  ...new Set([...DEFAULT_CORS_ALLOWED_ORIGINS, ...extraOrigins])
 ]
